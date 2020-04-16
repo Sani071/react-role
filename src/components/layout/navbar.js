575e7692bfc90ebc209de36ecbad117990025966
sani071 <saniahmed071@gmail.com>
2020-04-16 17:13:52 +0600

    FTR login button

--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import "./style.css";
+import { loginAction } from '../../store/action/actionCreator';
+import { useDispatch } from "react-redux";
 
 export default function Navbar() {
+    const dispatch = useDispatch();
+
     return (
         <div className="navbar_wrapper">
             <div>
                 <h1>
                     React Role App
             </h1>
+                <h3 onClick={ e => dispatch(loginAction(false)) }>
+                    Logout
+            </h3>
             </div>
 
         </div>
